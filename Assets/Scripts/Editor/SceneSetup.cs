0b8be2404a7f8d7c1f2737409c435a65e1c1893f
semimono <culleneason@gmail.com>
2016-09-29 22:48:09 -0600

    Automatic new scene setup. Improved script organization.

---
+++ Assets/Scripts/Editor/SceneSetup.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+
+[InitializeOnLoad]
+public class SceneSetup {
+
+	static SceneSetup() {
+		EditorApplication.hierarchyWindowChanged += freshInitialize;
+	}
+
+	private static void setup() {
+		// create player
+		Player player = createPrefab("Assets/Prefabs/Player.prefab").GetComponent<Player>();
+
+		// create alien
+		player.entity = createPrefab("Assets/Prefabs/Alien.prefab").GetComponent<Alien>();
+
+		// set lighting mode
+		Lightmapping.giWorkflowMode = Lightmapping.GIWorkflowMode.OnDemand;
+	}
+
+	private static void freshInitialize() {
+		// check that the level is fresh
+		GameObject[] gameObjects = GameObject.FindObjectsOfType<GameObject>();
+		if (gameObjects.Length != 1)
+			return;
+		List<GameObject> objectList = new List<GameObject>(gameObjects);
+		if (objectList.Find(go => go.name == "Main Camera") == null)
+			return;
+
+		// delete default objects
+		foreach (GameObject ob in gameObjects)
+			GameObject.DestroyImmediate(ob);
+
+		// create prefabs and do configuration
+		setup();
+	}
+
+	private static GameObject createPrefab(string assetPath, Transform parent=null) {
+		GameObject prefab = PrefabUtility.InstantiatePrefab(
+			AssetDatabase.LoadAssetAtPath<GameObject>(assetPath)) as GameObject;
+		prefab.transform.parent = parent;
+		return prefab;
+	}
+}
